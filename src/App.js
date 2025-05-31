d0102e7f15a1e663548fd0d0fb2c16a4d82cd898
Nada Moheyeldin <moheyeldeennada@gmail.com>
2025-06-01 00:12:21 +0300

    create NotFound page 404 and place its route path in App.js as a wild card at the end

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import MyForm from './components/form/form';
-
+import NotFound from './components/NotFound/NotFound';
 function App() {
   return (
     <div className="App">
@@ -11,6 +11,7 @@ function App() {
           <Switch>
             <Route path={"/"} component={Home} exact></Route>
             <Route path={"/login"} component={MyForm} exact></Route>
+            <Route path={"*"} component={NotFound} exact></Route>
         
           </Switch>
         </BrowserRouter>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div>
+      <h1 className="text-center text-danger">404 Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
+export default NotFound;
